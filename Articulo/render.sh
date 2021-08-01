6a5dae8255da6c0296028691582973de6783328c
lf <lifofernandez@gmail.com>
2021-08-01 19:31:48 -0300

    clean

--- Articulo/render.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bibtex ba-bycing # generates a bbl
-latex ba-bycing.tex # generates aux,dvi etc
-pdflatex ba-bycing.tex # generates a PDF
-
-# https://tex.stackexchange.com/questions/354196/bbl-file-not-generated
